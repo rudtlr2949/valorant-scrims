e47f394c4952ec97a2be7462f22e3700f997f9c9
rudtlr2949 <rudtlr2949@gmail.com>
2026-04-21 11:36:44 +0900

    Fix URLs for Haven and Drift map images

--- api/game-stats.js
+++ api/game-stats.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
   var MAP_IMAGES = {
     "Ascent":   "https://media.valorant-api.com/maps/7eaecc1b-4337-bbf6-6ab9-04b8f06b3319/splash.png",
     "Bind":     "https://media.valorant-api.com/maps/2c9d57ec-4431-9c5e-2939-8f9ef6dd5cba/splash.png",
-    "Haven":    "https://media.valorant-api.com/maps/2bee0dc9-4ffe-519b-1cbd-7825ad097fbf/splash.png",
+    "Haven":    "https://media.valorant-api.com/maps/2bee0dc9-4ffe-519b-1cbd-7fbe763a6047/splash.png",
     "Split":    "https://media.valorant-api.com/maps/d960549e-485c-e861-8d71-aa9d1aed12a2/splash.png",
     "Icebox":   "https://media.valorant-api.com/maps/e2ad5c54-4114-a870-9641-8ea21279579a/splash.png",
     "Breeze":   "https://media.valorant-api.com/maps/2fb9a4fd-47b8-4e7d-a969-74b4046ebd53/splash.png",
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
     "Lotus":    "https://media.valorant-api.com/maps/2fe4ed3a-450a-948b-6d6b-e89a78e680a9/splash.png",
     "Sunset":   "https://media.valorant-api.com/maps/92584fbe-486a-b1b2-9faa-39f2a00b33b1/splash.png",
     "Abyss":    "https://media.valorant-api.com/maps/224b0a95-48b9-f703-1bd8-67aca101a61f/splash.png",
-    "Drift":    "https://media.valorant-api.com/maps/de28aa9b-4cbe-1003-320e-6cb3ec657ae1/splash.png",
+    "Drift":    "https://media.valorant-api.com/maps/2c09d728-42d5-30d8-43dc-96a05cc7ee9d/splash.png",
     "District": "https://media.valorant-api.com/maps/690b81c8-4f7f-7046-9b9d-89e8f4ad9b25/splash.png",
     "Kasbah":   "https://media.valorant-api.com/maps/2c9d57ec-4431-9c5e-2939-8f9ef6dd5cba/splash.png",
     "Piazza":   "https://media.valorant-api.com/maps/fd267378-4d1d-484f-ff52-77821ed10dc2/splash.png",
